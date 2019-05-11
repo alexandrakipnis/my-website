7f17664a10fb167195f861fc73ae57d9a1d0c267
Sasha Kipnis <17sashak@gmail.com>
2019-05-11 15:14:49 -0700

    Updates

---
+++ precache-manifest.59159fb79ae9da7e810fd0f3930fd841.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "050b54078fbd9df6c0d6c85e82032651",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "2a39116e622cf64b30c3",
+    "url": "/my-website/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "3fd4e21f943c6602a746",
+    "url": "/my-website/static/js/2.3cca5092.chunk.js"
+  },
+  {
+    "revision": "2a39116e622cf64b30c3",
+    "url": "/my-website/static/js/main.c006879b.chunk.js"
+  },
+  {
+    "revision": "cde00616bb4e3308c99c",
+    "url": "/my-website/static/js/runtime~main.d755d5fd.js"
+  }
+]);
